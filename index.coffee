0aeb18a46c2c4524cc65c9b94bb188b50fde071a
Thomas Coats <github@voodoolabs.net>
2015-05-17 16:01:08 +1200

    Param structure

--- index.coffee
+++ index.coffee
@@ -1,10 +1,12 @@
 baby = require 'babyparse'
 
-module.exports = (exe, params) ->
-  getdata = exe.build params.__source
-  getparams = exe.build params.__params
-  (cb) ->
-    getparams (err, params) ->
-      getdata (err, data) ->
-        res = baby.parse data, params
-        cb null, res.data
+module.exports =
+  params:
+    csv: (exe, params) ->
+      getdata = exe.build params.__s
+      getparams = exe.build params.__p
+      (cb) ->
+        getparams (err, params) ->
+          getdata (err, data) ->
+            res = baby.parse data, params
+            cb null, res.data
--- index.js
+++ index.js
@@ -3,17 +3,21 @@ var baby;
 
 baby = require('babyparse');
 
-module.exports = function(exe, params) {
-  var getdata, getparams;
-  getdata = exe.build(params.__source);
-  getparams = exe.build(params.__params);
-  return function(cb) {
-    return getparams(function(err, params) {
-      return getdata(function(err, data) {
-        var res;
-        res = baby.parse(data, params);
-        return cb(null, res.data);
-      });
-    });
-  };
+module.exports = {
+  params: {
+    csv: function(exe, params) {
+      var getdata, getparams;
+      getdata = exe.build(params.__s);
+      getparams = exe.build(params.__p);
+      return function(cb) {
+        return getparams(function(err, params) {
+          return getdata(function(err, data) {
+            var res;
+            res = baby.parse(data, params);
+            return cb(null, res.data);
+          });
+        });
+      };
+    }
+  }
 };
